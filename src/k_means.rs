7b85adeee4cc64b7de9dc5ee6d4877a9210235af
Peter Hatch <petershatch@gmail.com>
2015-11-19 12:02:30 -0800

    Added centroid struct, made nodes and centroids initialization functions.

--- src/k_means.rs
+++ src/k_means.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-pub trait Data : Eq + Hash {
+pub trait Data : Eq + Hash + Copy + Clone {
     fn distance_to(&self, other: &Self) -> u64;
     fn mean_of(data_and_counts: &Vec<Node<Self>>) -> Self;
 }
@@ -11,21 +11,48 @@ pub struct Node<T: Data> {
     pub count: u32,
 }
 
+impl<T: Data> Node<T> {
+    fn to_centroid(&self) -> Centroid<T> {
+        Centroid { data: self.data }
+    }
+}
+
+struct Centroid<T: Data> {
+    data: T,
+}
+
 pub fn quantize<I>(items: I) -> HashMap<I::Item, I::Item>
+    where I: Iterator,
+          I::Item: Data {
+    let k = 256;
+
+    let nodes = create_nodes(items);
+    let mut centroids = initialize_centroids(k, &nodes);
+
+    println!("{:?}", nodes.len());
+    println!("{:?}", centroids.len());
+
+    let mut quantization_map = HashMap::new();
+    quantization_map
+}
+
+fn create_nodes<I>(items: I) -> Vec<Node<I::Item>>
     where I: Iterator,
           I::Item: Data {
 
     let mut count_of_items: HashMap<I::Item, u32> = HashMap::new();
-
     for item in items {
         let counter = count_of_items.entry(item).or_insert(0);
         *counter += 1;
     }
 
-    count_of_items.shrink_to_fit();
+    let mut nodes = Vec::with_capacity(count_of_items.len());
+    for (item, count) in count_of_items {
+        nodes.push(Node { data: item, count: count });
+    }
+    nodes
+}
 
-    println!("{:?}", count_of_items.len());
-
-    let mut quantization_map = HashMap::new();
-    quantization_map
+fn initialize_centroids<T: Data>(k: usize, nodes: &Vec<Node<T>>) -> Vec<Centroid<T>> {
+    nodes.iter().take(k).map(|node| { node.to_centroid() }).collect()
 }
