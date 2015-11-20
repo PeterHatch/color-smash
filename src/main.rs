2482f4272a1389020c393d95c5082a72d4cf14b2
Peter Hatch <petershatch@gmail.com>
2015-11-19 20:35:35 -0800

    Refactoring.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use std::path::Path;
 
 extern crate image;
-use image::{DynamicImage, GenericImage};
+use image::{GenericImage, RgbaImage};
 
 mod byte_utils;
 mod color;
@@ -16,19 +16,19 @@ mod tests;
 
 fn main() {
     let mut image = image::open(&Path::new("00.png")).unwrap();
+    let mut image = image.as_mut_rgba8().unwrap();
 
     let quantization_map = quantize_image(&image);
 
-    let mut output_image = image.as_mut_rgba8().unwrap();
-    for pixel in output_image.pixels_mut() {
+    for pixel in image.pixels_mut() {
         let &new_color = quantization_map.get(pixel).unwrap();
         *pixel = new_color;
     }
 
-    output_image.save("output.png");
+    image.save("output.png");
 }
 
-fn quantize_image(image: &DynamicImage) -> HashMap<Color, Color> {
-    let colors = image.pixels().map(|(_, _, color)| color);
+fn quantize_image(image: &RgbaImage) -> HashMap<Color, Color> {
+    let colors = image.pixels().map(|&color| color);
     k_means::quantize(colors)
 }
